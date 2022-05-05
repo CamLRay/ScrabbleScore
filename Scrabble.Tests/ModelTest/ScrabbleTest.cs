e0f70844e8c0d604e49a99aa4fde10c6606f2a06
James Fox <jimmyfox00@gmail.com>
2022-05-05 15:22:36 -0700

    Added number project
    Co-authored-by: Cameron Ray <crayiphone@gmail.com>

--- Scrabble.Tests/ModelTest/ScrabbleTest.cs
+++ Scrabble.Tests/ModelTest/ScrabbleTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using ScrabbleScore;
+using NumberToWords;
 
 namespace ScrabbleScore.Tests
 {
@@ -19,5 +20,61 @@ namespace ScrabbleScore.Tests
       Scrabble testWord = new Scrabble("Supercalifragilisticexpialidocious");
       Assert.AreEqual(56, testWord.GetScoreDictionary());
     }
+    [TestMethod]
+    public void NumberTranslator_TranslatesSignleNumberToWord_Srting()
+    {
+      Assert.AreEqual("two", Number.NumberTranslator(2));
+    }
+    [TestMethod]
+    public void NumberTranslator_TranslatesTwoDigitNumberToWord_String()
+    {
+      Assert.AreEqual("twenty-two", Number.NumberTranslator(22));
+    }
+     [TestMethod]
+    public void LargeNumberTranslator_TranslatesThreeDigitNumberToWord_String()
+    {
+      Assert.AreEqual("one hundred twenty-one", Number.LargeNumberTranslator(121));
+    }
+    [TestMethod]
+    public void LargeNumberTranslator_TranslatesFourDigitNumberToWord_String()
+    {
+      Assert.AreEqual("one thousand one hundred twenty-one", Number.LargeNumberTranslator(1121));
+    }
+    [TestMethod]
+    public void LargeNumberTranslator_TranslatesFiveDigitNumberToWord_String()
+    {
+      Assert.AreEqual("ten thousand one hundred twenty-one", Number.LargeNumberTranslator(10121));
+    }
+    [TestMethod]
+    public void LargeNumberTranslator_TranslatesSixDigitNumberToWord_String()
+    {
+      Assert.AreEqual("one hundred twenty-three thousand one hundred twenty-one", Number.LargeNumberTranslator(123121));
+    }
+    [TestMethod]
+    public void LargeNumberTranslator_TranslatesSevenDigitNumberToWord_String()
+    {
+      Assert.AreEqual("one million one hundred twenty-one", Number.LargeNumberTranslator(1000121));
+    }
+    [TestMethod]
+    public void LargeNumberTranslator_TranslatesEightDigitNumberToWord_String()
+    {
+      Assert.AreEqual("ten million one hundred twenty-one", Number.LargeNumberTranslator(10000121));
+    }
+    [TestMethod]
+    public void LargeNumberTranslator_TranslatesNineDigitNumberToWord_String()
+    {
+      Assert.AreEqual("one hundred million one hundred twenty-one", Number.LargeNumberTranslator(100000121));
+    }
+    [TestMethod]
+    public void LargeNumberTranslator_TranslatesTenDigitNumberToWord_String()
+    {
+      Assert.AreEqual("one billion one hundred twenty-one", Number.LargeNumberTranslator(1000000121));
+    }
+    [TestMethod]
+    public void LargeNumberTranslator_TranslatesThirteenDigitNumberToWord_String()
+    {
+      Assert.AreEqual("one trillion one hundred twenty-one", Number.LargeNumberTranslator(1000000000121));
+    }
+
   }
 }
---
+++ Scrabble/Models/Numbers.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace NumberToWords
+{
+  public class Number{
+    private static Dictionary<long, string> _singleDigitDictionary = new Dictionary<long, string>() {
+      {1, "one"}, 
+      {2, "two"}, 
+      {3, "three"}, 
+      {4, "four"}, 
+      {5, "five"}, 
+      {6, "six"}, 
+      {7, "seven"}, 
+      {8, "eight"}, 
+      {9, "nine"}, 
+      {10, "ten"},
+      {11, "eleven"},
+      {12, "twelve"},
+      {13, "thirteen"},
+      {14, "fourteen"},
+      {15, "fifteen"},
+      {16, "sixteen"},
+      {17, "seventeen"},
+      {18, "eighteen"},
+      {19, "nineteen"}}; 
+    private static Dictionary<long, string> _TensDigitDictionary = new Dictionary<long, string>() {
+      {1, "ten"},
+      {2, "twenty"}, 
+      {3, "thirty"},
+      {4, "fourty"},
+      {5, "fifty"},
+      {6, "sixty"},
+      {7, "seventy"},
+      {8, "eighty"},
+      {9, "ninety"}};
+
+    private static Dictionary<long, string> _FoldDigitDictionary = new Dictionary<long, string>() {
+      {100, "hundred"},
+      {1000, "thousand"}, 
+      {1000000, "million"},
+      {1000000000, "billion"},
+      {1000000000000, "trillion"}};
+      
+      // 1,100,101 one million one hundred thousand one hundred one
+
+// 1121
+    public static string NumberTranslator(long number)
+    {
+      string numberString = "";
+      if(number > 0)
+      {
+        if(number < 20)
+        {
+          numberString += _singleDigitDictionary[number];
+        }
+        else if (number < 100)
+        {
+          numberString += _TensDigitDictionary[number/10];
+          if(number % 10 > 0)
+          {
+            numberString += "-" + _singleDigitDictionary[number % 10];
+          }
+        }
+      }
+      return numberString;
+    }
+// 100,000,121
+    public static string LargeNumberTranslator(long number)
+    {
+      List<string> stringList = new List<string>() {};
+      if(number == 0)
+      {
+        return "zero";
+      }
+      for (int i = _FoldDigitDictionary.Count -1; i >= 0; i--)
+      {
+        if(number >= _FoldDigitDictionary.ElementAt(i).Key)
+        {
+          if(number/(_FoldDigitDictionary.ElementAt(i).Key) >= 100){
+            long tempNum = number/(_FoldDigitDictionary.ElementAt(i).Key);
+            stringList.Add(NumberTranslator(tempNum/100) + " hundred");
+            stringList.Add(NumberTranslator(tempNum%100));
+          }
+          stringList.Add(NumberTranslator(number/_FoldDigitDictionary.ElementAt(i).Key));
+          stringList.Add(_FoldDigitDictionary.ElementAt(i).Value);
+          stringList.Add(NumberTranslator(number%(_FoldDigitDictionary.ElementAt(i).Key)));
+          number%=(_FoldDigitDictionary.ElementAt(i).Key);
+        }
+        else {
+          stringList.Add(NumberTranslator(number));
+        }
+      }
+      string numberString = "";
+      foreach(string word in stringList)
+      {
+        if(word != "")
+        {
+          numberString += word + " ";
+        }
+      }
+      return numberString.Trim();
+    }
+
+  }
+}
